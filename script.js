6ef00b9a2dc80448d0f262f382c2e2933098760f
namrata <namrataap725@gmail.com>
2025-05-13 20:21:48 +0545

    getting word info

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 
-    const toggleBtn = document.getElementById('modeToggleBtn');
+const toggleBtn = document.getElementById('modeToggleBtn');
 
-    toggleBtn.addEventListener('click', () => {
-        document.body.classList.toggle('dark');
-        const isDark = document.body.classList.contains('dark');
-        toggleBtn.textContent = isDark ? ' Light Mode' : ' Dark Mode';
-    });
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const isDark = document.body.classList.contains('dark');
+    toggleBtn.textContent = isDark ? ' Light Mode' : ' Dark Mode';
+});
 
+
+const wordInput = document.querySelector('.wordInput');
+const enterWord = document.querySelector('.enterWord');
+const Search = document.querySelector('.Search');
+
+
+
+
+const getData = async() => {
+    const wordInfo =enterWord.value.trim();
+    if(!wordInfo){
+        alert('please Enter a Word');
+    }
+    const response= await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordInfo}`);
+    const data= await response.json();
+    console.log(data)
+}
+
+
+
+Search.addEventListener('click',getData);
